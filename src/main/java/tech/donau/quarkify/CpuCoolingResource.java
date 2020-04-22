94d1ba609c6eae1593ca4b7ce55718285a82eb1b
dmi3coder <dmi3coder@gmail.com>
2020-04-22 18:42:17 +0300

    Simple cooling example

--- src/main/java/tech/donau/quarkify/CpuCoolingResource.java
+++ src/main/java/tech/donau/quarkify/CpuCoolingResource.java
@@ -1,16 +1,26 @@
 package tech.donau.quarkify;
 
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.net.URL;
 
 @Path("/cooling")
 public class CpuCoolingResource {
+    public static Context context = Context.newBuilder().allowIO(true).allowAllAccess(true).build();
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public String hello() throws IOException {
+        final URL cpuCoolingR = getClass().getClassLoader().getResource("cpu_cooling.R");
+        System.out.println(cpuCoolingR);
+        Value needs_cooling = context.eval(Source.newBuilder("r", cpuCoolingR).build());
+        return needs_cooling.asDouble() > 0.8 ? "Enable cooling" : "No need to enable cooling";
     }
 }
