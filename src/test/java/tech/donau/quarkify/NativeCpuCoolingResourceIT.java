da8b8fd46cf573666df56f35f197a9f12daace8d
Dmytro Chaban <dmi3coder@gmail.com>
2020-04-22 15:35:59 +0000

    Initial commit

---
+++ src/test/java/tech/donau/quarkify/NativeCpuCoolingResourceIT.java
@@ -0,0 +1,9 @@
+package tech.donau.quarkify;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCpuCoolingResourceIT extends CpuCoolingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
