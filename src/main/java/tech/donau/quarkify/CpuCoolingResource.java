d02c57dc0a22bc4aa8083730b26c10cbc624cd18
dmi3coder <dmi3coder@gmail.com>
2020-04-22 19:50:15 +0300

    Extracted cpu_cooling.R execution to startup listener

--- src/main/java/tech/donau/quarkify/CpuCoolingResource.java
+++ src/main/java/tech/donau/quarkify/CpuCoolingResource.java
@@ -1,7 +1,9 @@
 package tech.donau.quarkify;
 
+import io.quarkus.runtime.StartupEvent;
 import org.graalvm.polyglot.*;
 
+import javax.enterprise.event.Observes;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
@@ -11,11 +13,14 @@ import java.net.URL;
 public class CpuCoolingResource {
     public static Context context = Context.newBuilder().allowIO(true).allowAllAccess(true).build();
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@QueryParam("grads") Double grads) throws IOException {
+    public void onStart(@Observes StartupEvent e) throws IOException {
         final URL cpuCoolingR = getClass().getClassLoader().getResource("cpu_cooling.R");
         context.eval(Source.newBuilder("R", cpuCoolingR).build());
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@QueryParam("grads") Double grads) {
         final Value needs_cooling = context.getBindings("R").getMember("needs_cooling");
         final Value result = needs_cooling.execute(grads);
         return result.asDouble() > 0.8 ? "Enable cooling" : "No need to enable cooling";
